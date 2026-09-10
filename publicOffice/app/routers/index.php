b6e9b4eb30eace54de5e7eab51134700668ae9c8
Rulia914 <falconejulie@gmail.com>
2026-09-10 14:42:21 +0200

    Page contact

--- publicOffice/app/routers/index.php
+++ publicOffice/app/routers/index.php
@@ -1,11 +1,22 @@
 <?php
+//PAGE CONTACT
+//PATTERN : ?contact
+//URL : 
+//CTRL : 
+//ACTION : 
+if(isset($_GET['contact']) ):
+    $title = "Contact";
+    ob_start();
+    include_once '../app/views/templates/partials/_contact.php';
+    $content = ob_get_clean();
+
 
 //DETAIL POST.show
 //PATTERN : /posts/id/slug
 //URL : ?posts=show&id=x
 //CTRL : postsController
 //ACTION : showAction
-if(isset($_GET['posts']) ):
+elseif(isset($_GET['posts']) ):
     include_once '../app/routers/posts.php';
 
 //Route par défaut : les dix derniers posts
